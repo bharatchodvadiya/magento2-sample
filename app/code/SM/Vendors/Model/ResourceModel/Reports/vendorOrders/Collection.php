a22de4d2f4f3c7e65f230c0af9578e121361562f
Bharat Chodvadiya <bharat.chodvadiya@gmail.com>
2019-01-02 23:45:23 +0530

    Add files via upload

---
+++ app/code/SM/Vendors/Model/ResourceModel/Reports/vendorOrders/Collection.php
@@ -0,0 +1,7 @@
+<?php
+namespace SM\Vendors\Model\ResourceModel\Reports\vendorOrders\Updatedat;
+
+class Collection extends \SM\Vendors\Model\ResourceModel\Reports\vendorOrders\Collection
+{
+	protected $_type = 'updated_at';
+}
